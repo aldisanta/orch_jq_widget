be94db0c2bfeb44a5d6a93b3ce9ec6baf22d4c50
aldisanta <aldisanta@gmail.com>
2015-04-22 04:06:05 -0400

    Changed numeric mask on portal based on maxlength

--- masking.js
+++ masking.js
@@ -3,7 +3,11 @@ var $masking = $masking || {};
 $masking.masking = function () {
 	$('.mask-year').mask('9999');
 	$('.mask-gpa').mask('9.99');
-	$('.mask-numeric').mask('09');
+	var mask = 0;
+	$('.mask-numeric').each(function(index, el) {
+		mask = Array($(el).prop('maxlength') + 1).join('0');
+		$(this).mask(mask);
+	});
 	$('.mask-zip').mask('00000-000');
 	$('.mask-phone-input').mask('999-999-9999');
 	$('.phone-input').unmask('999-999-9999');
